ee9c04481b099b9930e2da36f4e0421f7d3af56e
sajidul-dev <mdsajidulalam1245@gmail.com>
2023-11-28 00:47:42 +0600

    Isse fixed

--- pages/webstore/Product.js
+++ pages/webstore/Product.js
@@ -5,19 +5,19 @@ const Product = ({ product }) => {
     <div className=" w-full max-w-[15rem] h-[20rem] rounded overflow-hidden shadow-lg ml-3">
       <img
         className="w-full h-40 object-cover mb-2"
-        src={product.image}
-        alt={product.title}
+        src={product?.image}
+        alt={product?.title}
       />
       <div className="px-6 py-4 bg-gray-800 text-white transition duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-600">
         <div className="font-semibold text-center text-lg mb-0">
-          {product.title}
+          {product?.title}
         </div>
         <p className="text-center text-base h-40">
-          ID: {product.id}
+          ID: {product?.id}
           <br />
-          Price: ${product.price}
+          Price: ${product?.price}
           <br />
-          Promo Code: {product.promoCode}
+          Promo Code: {product?.promoCode}
           {/* <div className="card-actions">
             <button className="btn btn-primary">Buy Now</button>
           </div> */}
