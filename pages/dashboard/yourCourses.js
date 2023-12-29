4e56d7d7cb8a36c4d6d619fb11d2db3ca885b25a
sajidul-dev <mdsajidulalam1245@gmail.com>
2023-12-29 12:25:57 +0600

    Deploy error solved

--- pages/dashboard/yourCourses.js
+++ pages/dashboard/yourCourses.js
@@ -23,7 +23,7 @@ const YourCoursesPage = () => {
   return (
     <DashboardLayout>
       <h1 className="text-2xl text-[#D700E2] font-semibold mb-3 border-b border-[#A5009B] pb-4">
-        Welcome {user.name} , Ready For Your Next Lesson?
+        Welcome {user?.name} , Ready For Your Next Lesson?
       </h1>
 
       <div className="flex flex-col gap-10 my-5">
@@ -32,13 +32,15 @@ const YourCoursesPage = () => {
             key={c._id}
             className="flex bg-[#160030] border-2 border-[#A5009B] rounded-md p-3 h-[300px]">
             <div className="self-center w-[40%]">
-              <img src={c.thumbnail} alt="" />
+              <img src={c?.thumbnail} alt="" />
             </div>
             <div className="w-[60%] flex flex-col justify-evenly">
-              <div className="text-3xl text-[#A300B0] font-bold">{c.title}</div>
+              <div className="text-3xl text-[#A300B0] font-bold">
+                {c?.title}
+              </div>
               <div className="text-[#D400F7] text-[20px]">
                 <span className="text-base">Instructed By :</span>{" "}
-                {c.instructor}
+                {c?.instructor}
               </div>
               <div className="flex items-center justify-evenly self-start gap-10">
                 <span className="cursor-pointer bg-[#D700E2] px-5 py-2 text-white rounded-md font-semibold">
