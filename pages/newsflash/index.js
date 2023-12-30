e8458077f836fdfe1d6772e467e8aff5375c822f
Anik Deb <anikdebcse@gmail.com>
2023-12-31 02:39:17 +0600

    dynamic newsflash

--- pages/newsflash/index.js
+++ pages/newsflash/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BiSolidLike } from "react-icons/bi";
 import { BiSolidDislike } from "react-icons/bi";
 import { FaCommentAlt } from "react-icons/fa";
@@ -8,40 +8,50 @@ import { MdPhotoCamera } from "react-icons/md";
 import { HiMiniDocumentPlus } from "react-icons/hi2";
 import Header from "@/components/shared/Navbar/Header";
 import Layout from "@/components/layout/Layout";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Newsflash = () => {
-  const [likeCount, setLikeCount] = useState(120);
-  const [dislikeCount, setDislikeCount] = useState(20);
-  const [commentCount, setCommentCount] = useState(80);
-  const [shareCount, setShareCount] = useState(30);
+  const [posts, setPosts] = useState([]);
 
-  const handleLike = () => {
-    setLikeCount((prevCount) => prevCount + 1);
+  const user = useSelector((state) => state.userReducer.user);
+
+  const dateFormat = (inputDateTime) => {
+    const dateObject = new Date(inputDateTime);
+
+    const formattedDateTime = dateObject.toLocaleString("en-US", {
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    return formattedDateTime;
   };
 
-  const handleDislike = () => {
-    setDislikeCount((prevCount) => prevCount + 1);
-  };
+  useEffect(() => {
+    axios
+      .get("/api/user/post")
+      .then((res) => {
+        setPosts(res.data.post);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  const handleComment = () => {
-    setCommentCount((prevCount) => prevCount + 1);
-  };
-
-  const handleShare = () => {
-    setShareCount((prevCount) => prevCount + 1);
-  };
-
+  console.log(posts);
   return (
     <Layout>
       <div className="flex-basis-47 bg-gray-200">
         <div className="w-full bg-white rounded p-5 text-gray-700">
           <div className="flex items-center">
             <img
-              src="/images/profile-pic.jpg"
+              src={user?.profilePic ? user.profilePic : "/images/children.jpg"}
               className="w-12 h-12 rounded-full mr-2"
             />
             <div>
-              <p className="font-semibold mb-0 text-gray-700">Username</p>
+              <p className="font-semibold mb-0 text-gray-700">{user.name}</p>
               <small className="text-xs">
                 Public <i className="fas fa-caret-down"></i>
               </small>
@@ -97,70 +107,70 @@ const Newsflash = () => {
             </div>
           </div>
         </div>
-        <div className="bg-white rounded p-4 my-5">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <img
-                src="/images/profile-pic.jpg"
-                className="w-10 h-10 rounded-full mr-2"
-              />
-              <div>
-                <p className="font-semibold mb-0 text-gray-700">Username</p>
-                <span className="text-xs text-gray-500">
-                  September 22, 2023, 23:19 pm
-                </span>
+        {posts.map((post) => (
+          <div className="bg-white rounded p-4 my-5">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <img
+                  src={
+                    post?.user?.profilePic
+                      ? post?.user?.profilePic
+                      : "/images/children.jpg"
+                  }
+                  className="w-10 h-10 rounded-full mr-2"
+                />
+                <div>
+                  <p className="font-semibold mb-0 text-gray-700">
+                    {post?.user?.name}
+                  </p>
+                  <span className="text-xs text-gray-500">
+                    {dateFormat(post?.createdAt)}
+                  </span>
+                </div>
+              </div>
+              <a href="#" className="text-gray-700">
+                <i className="fas fa-ellipsis-v"></i>
+              </a>
+            </div>
+            <p className="text-gray-500 text-lg mb-5">
+              {post?.caption}
+              {/* <span className="text-gray-700 font-semibold">EduQuanta</span>
+              Spread your thoughts to our community.
+              <a href="#" className="text-blue-500">
+                #Easy Tutorials
+              </a>
+              <a href="#" className="text-blue-500">
+                #e-learning platform
+              </a> */}
+            </p>
+            <img
+              src={`${post?.photos ? post?.photos : "/images/feed-image.png"}`}
+              className=" rounded mb-3 w-1/2 mx-auto h-[600px]"
+            />
+            <div className=" flex items-center justify-between">
+              <div className="flex space-x-20 ">
+                <div className="flex items-center mr-5 mb-2">
+                  <BiSolidLike className="text-lg mr-2" />
+                  23
+                </div>
+
+                <div className="flex items-center mr-5 mb-2">
+                  <FaCommentAlt className="text-lg mr-2" />2
+                </div>
+                <div className="flex items-center mr-5 mb-2">
+                  <FaShare className="text-lg mr-2" />4
+                </div>
+              </div>
+              <div className="flex items-center">
+                <img
+                  src="/images/profile-pic.jpg"
+                  className="w-5 h-5 rounded-full mr-2"
+                />
+                <i className="fa-solid fa-caret-down"></i>
               </div>
             </div>
-            <a href="#" className="text-gray-700">
-              <i className="fas fa-ellipsis-v"></i>
-            </a>
           </div>
-          <p className="text-gray-500 text-lg mb-5">
-            Welcome to our open learning platform{" "}
-            <span className="text-gray-700 font-semibold">EduQuanta</span>
-            Spread your thoughts to our community.
-            <a href="#" className="text-blue-500">
-              #Easy Tutorials
-            </a>
-            <a href="#" className="text-blue-500">
-              #e-learning platform
-            </a>
-          </p>
-          <img src="/images/feed-image.png" className="w-full rounded mb-2" />
-          <div className=" flex items-center justify-between">
-            <div className="flex space-x-20 ">
-              <div className="flex items-center mr-5 mb-2" onClick={handleLike}>
-                <BiSolidLike className="text-lg mr-2" />
-                {likeCount}
-              </div>
-              <div
-                className="flex items-center mr-5 mb-2"
-                onClick={handleDislike}>
-                <BiSolidDislike className="text-lg mr-2" />
-                {dislikeCount}
-              </div>
-              <div
-                className="flex items-center mr-5 mb-2"
-                onClick={handleComment}>
-                <FaCommentAlt className="text-lg mr-2" />
-                {commentCount}
-              </div>
-              <div
-                className="flex items-center mr-5 mb-2"
-                onClick={handleShare}>
-                <FaShare className="text-lg mr-2" />
-                {shareCount}
-              </div>
-            </div>
-            <div className="flex items-center">
-              <img
-                src="/images/profile-pic.jpg"
-                className="w-5 h-5 rounded-full mr-2"
-              />
-              <i className="fa-solid fa-caret-down"></i>
-            </div>
-          </div>
-        </div>
+        ))}
         <button
           type="button"
           className="load-more-btn bg-blue-500 text-white p-2 rounded">
