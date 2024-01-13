3b5601022d528d00f04aff1a2fcb947a9f286661
JonyDebCSE-21 <jonydeb02107092@gmail.com>
2024-01-14 01:45:39 +0600

    career

--- pages/footerPage/career/index.js
+++ pages/footerPage/career/index.js
@@ -1,7 +1,48 @@
 import React from "react";
 
 const Career = () => {
-  return <div>Career</div>;
+  return (
+    <div className="container mx-auto mt-2 p-6 bg-white rounded shadow-md">
+      <h1 className="text-3xl font-bold mb-3">Career opportunity</h1>
+      <p className=" text-justify mb-4">Last updated: [26-11-2023]</p>
+      <h4 className="text-xl font-bold mb-2">Join Our Team</h4>
+      <p className=" text-justify mb-4">
+        At [EduQuanta], we are on a mission to revolutionize education, empower
+        learners, make career opportunities etc. We believe in the
+        transformative power of education and are committed to providing an
+        exceptional learning experience for individuals around the globe.{" "}
+      </p>
+      <h4 className="text-xl font-bold mb-2"> Why Join [EduQuanta]?</h4>
+      <p className=" text-justify mb-4">
+        {" "}
+        Impact: Be a part of a team that is making a real difference in the
+        world of education. Contribute to the growth and success of our
+        innovative e-learning platform. <br />
+        Collaboration: Work with a diverse and talented group of individuals who
+        are passionate about education and technology. Collaborate in a dynamic
+        and inclusive environment where your ideas are valued.
+        <br /> Growth Opportunities: We invest in the growth and development of
+        our team members. Whether you are an experienced professional or just
+        starting your career, there are opportunities for learning and
+        advancement.
+        <br />
+        Innovation: Embrace the opportunity to work on cutting-edge projects and
+        contribute to the continuous improvement of our platform. We encourage
+        creative thinking and value fresh perspectives.
+      </p>
+      <h4 className="text-xl font-bold mb-2">Current Open Positions: </h4>
+      Excel expert: [Needs to manage all of the calculation in the website and
+      make spreadsheet] <br />
+      Designer: [An expert web designer for designig work of the website] <br />
+      Instructor:[Expert instructor to lead the learning prcess]
+      <h6 className="text-xl font-bold mb-2">How to Apply:</h6>
+      Interested in joining our team? We'd love to hear from you! To apply,
+      please send your resume and a cover letter to [elearningpciu@gmail.com].
+      Be sure to include the position title in the subject line. [EduQuanta] is
+      an equal opportunity employer and we encourage candidates from all
+      backgrounds to apply.
+    </div>
+  );
 };
 
 export default Career;
